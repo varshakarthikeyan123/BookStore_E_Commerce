4a159d98cf0f6c27d407ea53b589a5c16815e62b
varshakarthikeyan123 <varshakarthikeyan1@gmail.com>
2026-04-28 14:08:40 +0530

    [Varsha] :  Added : Config Server with Git repo integration

---
+++ config-server/src/main/java/com/bookstore/config_server/ConfigServerApplication.java
@@ -0,0 +1,18 @@
+package com.bookstore.config_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @EnableConfigServer → enables centralized configuration
+ * All microservices will fetch configs from here
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+}

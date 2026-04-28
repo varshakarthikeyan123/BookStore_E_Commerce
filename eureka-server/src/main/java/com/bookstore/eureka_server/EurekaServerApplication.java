234f54e15af2186aadfa13df61be7002a40f75b2
varshakarthikeyan123 <varshakarthikeyan1@gmail.com>
2026-04-28 11:17:01 +0530

    [Varsha] :  Added : Eureka Server project setup from scratch with configuration

---
+++ eureka-server/src/main/java/com/bookstore/eureka_server/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package com.bookstore.eureka_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Entry point of Eureka Server
+ * @EnableEurekaServer → converts app into service registry
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}
